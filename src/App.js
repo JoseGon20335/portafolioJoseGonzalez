19090d57aceaaa8d4f95c92e32087d228c54ed8b
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-05-28 16:45:08 -0600

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
+import BotPart from './component/bot';
 
 const App = () => {
-
-  
 
   return (
     <div>
       <p>HOLA</p>
+      <BotPart/>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/component/bot.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import '../App.css';
+
+const BotPart = () => {
+
+  return (
+    <div className='bot'>
+      <div className="LeftLine"></div>
+        <p>botones</p>
+      <div className="RightLine"></div>
+    </div>
+  );
+}
+
+export default BotPart;
