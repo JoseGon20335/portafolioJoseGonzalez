da050944de3180493b58fc3da9502e2d1a949c75
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-05-30 02:39:50 -0600

    diablo

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -1,35 +1,81 @@
 import React, { useState } from 'react';
 import '../App.css';
-import arrow from '../image/arrow.svg'
-import lg from '../image/roku.svg'
+import arrow from '../image/arrow.svg';
+import lg from '../image/roku.svg';
+import { project_list } from './projectList';
 
 const ProjectPart = () => {
 
+  const [k, setK] = useState(0);
+  const [title, setTitle] = useState(project_list[0].name);
+  const [desc, setDesc] = useState(project_list[0].des);
+  const [image, setImage] = useState(project_list[0].img);
+  const [work, setWork] = useState(project_list[0].work);
+  const [time, setTime] = useState(project_list[0].time);
+  const [habi, setHabi] = useState(project_list[0].habilidades);
+
+  const clickHandler = e => {
+
+    let largo = project_list.length;
+
+    console.log("largo", largo, "k", k)
+
+    if(e.target.name == 'resta'){
+      if( k < 0){
+        console.log("1")
+        setK(largo)
+      } else{
+        console.log("2")
+        let val = k - 1;
+        setK(val)
+      }
+    } else if(e.target.name == 'suma'){
+      if( k > largo){
+        console.log("3")
+        setK(0)
+      } else{
+        console.log("4")
+        let val = k + 1;
+        setK(val)
+      }
+    }
+
+    console.log("largo2", largo, "k2", k)
+
+    setTitle(project_list[k].name);
+    setImage(project_list[k].img);
+    setDesc(project_list[k].des);
+    setWork(project_list[k].work);
+    setTime(project_list[k].time);
+    setHabi(project_list[k].habilidades);
+
+  }
+
   return (
     <div className='projectContainer'>
-      <div className='leftArrow'>
-        <img src={arrow}/>
+      <div className='leftArrow' name='resta' onClick={clickHandler}>
+        <img name='resta' src={arrow}/>
       </div>
       <div className='projectContent'>
         <div className='caja'>
           <div className='cajaLeft'>
-            <p>AHA</p>
-            <img src={lg}/>
+            <p>{title}</p>
+            <img src={image}/>
           </div>
           <div className='cajaRight'>
-            <p>Trabajo en AHA, plataforma de streaming, en la que trabaje como desarrollador de frontend para los dispositivos de roku, programando en brightscript</p>
+            <p>{desc}</p>
             
             <div className='cajasPro'>
-              <div className='bottomCube'>Time</div>
-              <div className='bottomCube'>Work</div>
-              <div className='bottomCube'>Habilities</div>
+              <div className='bottomCube'>{time}</div>
+              <div className='bottomCube'>{work}</div>
+              <div className='bottomCube'>{habi}</div>
             </div>
             
           </div>
         </div>
       </div>
-      <div className='rigthArrow'>
-      <img src={arrow}/>
+      <div className='rigthArrow' name='suma' onClick={clickHandler}>
+        <img name='suma' src={arrow}/>
       </div>
     </div>
   );
---
+++ src/component/projectList.js
@@ -0,0 +1,77 @@
+import num1 from '../image/react.svg';
+import num2 from '../image/angular.svg';
+import num10 from '../image/roku.svg';
+import num3 from '../image/javas.svg';
+import num4 from '../image/java.svg';
+import num5 from '../image/py.svg';
+import num6 from '../image/css.svg';
+import num7 from '../image/unity.svg';
+import num8 from '../image/android.svg';
+import num9 from '../image/lg.svg';
+
+export const project_list = [
+    {
+        key: 0,
+        name: 'AHA',
+        img: num10,
+        des: 'Trabajo en AHA, plataforma de streaming, en la que trabaje como desarrollador para los dispositivos de roku, programando en brightscript.',
+        time: '3 meses',
+        work: 'frontend dev',
+        habilidades: 'Brightscript'
+    },
+    {
+        key: 1,
+        name: 'STRUMM',
+        img: num10,
+        des: 'Trabajo en STRUMM, plataforma de streaming, en la que trabaje como desarrollador para los dispositivos de roku, programando en brightscript, creando eventos para enviar a base de datos.',
+        time: '1 mes',
+        work: 'frontend dev',
+        habilidades: 'Brightscript'
+    },
+    {
+        key: 2,
+        name: 'PANTAYA',
+        img: num2,
+        des: 'Trabajo en PANTAYA, plataforma de streaming, en la que trabaje como desarrollador encargado de la creacion de multiples secciones y hacer refactor al proyecto. Proyecto desarrollado en LG',
+        time: '2 meses',
+        work: 'frontend dev',
+        habilidades: 'Angular'
+    },
+    {
+        key: 3,
+        name: 'WHITELABEL',
+        img: num10,
+        des: 'Trabajo en whitelabel creando la base para ser usada en proyectos futuros, buscando un codigo pulido y solido. Desarrollo en web y smartv.',
+        time: '1 mes',
+        work: 'frontend dev',
+        habilidades: 'Angular/React'
+    },
+    {
+        key: 4,
+        name: 'PORTAFOLIO',
+        img: num1,
+        des: 'Creacion del mi propio portafolio creando todos los elementos y arquitectura en React.',
+        time: '1 semanas',
+        work: 'fullstack',
+        habilidades: 'React'
+    },
+    {
+        key: 5,
+        name: 'ECOMERCE',
+        img: num1,
+        des: 'Proyecto desarrollado en conjunto con la seccion del profesor Ludwing, trabajando en el area de inventario, creando la conexion y extraccion de datos.',
+        time: '3 semanas',
+        work: 'fullstack',
+        habilidades: 'React'
+    },
+    {
+        key: 6,
+        name: 'U-Learn',
+        img: num1,
+        des: 'Trabajando el proyecto de ing. software, con una aplicacion para estudiantes, encargado de la seccion de calendario.',
+        time: '3 meses',
+        work: 'fullstack',
+        habilidades: 'React'
+    }
+  ];
+  
