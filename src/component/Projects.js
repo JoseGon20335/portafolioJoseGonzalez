2417f0bb48d2cd5625194c95a605f2e767829f37
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-05-29 23:02:22 -0600

    css

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -1,18 +1,34 @@
 import React, { useState } from 'react';
 import '../App.css';
 import arrow from '../image/arrow.svg'
+import lg from '../image/roku.svg'
 
 const ProjectPart = () => {
 
   return (
     <div className='projectContainer'>
-      <div className='rigthArrow'>
+      <div className='leftArrow'>
         <img src={arrow}/>
       </div>
       <div className='projectContent'>
-        <p>hola</p>
+        <div className='caja'>
+          <div className='cajaLeft'>
+            <p>AHA</p>
+            <img src={lg}/>
+          </div>
+          <div className='cajaRight'>
+            <p>Trabajo en AHA, plataforma de streaming, en la que trabaje como desarrollador de frontend para los dispositivos de roku, programando en brightscript</p>
+            
+            <div className='cajasPro'>
+              <div className='bottomCube'>Time</div>
+              <div className='bottomCube'>Work</div>
+              <div className='bottomCube'>Habilities</div>
+            </div>
+            
+          </div>
+        </div>
       </div>
-      <div className='leftArrow'>
+      <div className='rigthArrow'>
       <img src={arrow}/>
       </div>
     </div>
