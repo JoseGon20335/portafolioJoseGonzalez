75274e741c22a4038ff7171fdb15c119dbea5504
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-06-01 03:57:00 -0600

    file

--- src/component/Links.js
+++ src/component/Links.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
 import '../App.css';
+import git from '../image/git.svg';
+import cv from '../image/cv.svg';
 
 const LinksPart = () => {
 
+  const CVLink = () => {}
+
+  const GitLink = () => {}
+
   return (
-    <div>
-      <p>LINKS!</p>
+    <div className='links'>
+      <div className='git'>
+        <img src={git}/>
+        <p onClick={GitLink}>GITHUB</p>
+      </div>
+      <div className='git'>
+        <img src={cv}/>
+        <p onClick={CVLink}>DOWNLOAD CV</p>
+      </div>
     </div>
   );
 }
