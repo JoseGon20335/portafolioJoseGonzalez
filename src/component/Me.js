4480454c6400b7d7742b01b0c6dc979c589d46ad
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-05-31 20:47:55 -0600

    me

--- src/component/Me.js
+++ src/component/Me.js
@@ -11,6 +11,8 @@ import { skill_list } from './skillsList';
 
 const MePart = () => {
 
+  const [op, setOp] = useState("mySelf");
+
   const [image, setImage] = useState(skill_list[0].img);
   const [title, setTitle] = useState(skill_list[0].key);
   const [desc, setDesc] = useState(skill_list[0].des);
@@ -29,11 +31,11 @@ const MePart = () => {
 
   const Perks = () => {
     return (
-      <div className='contentSkills'>
-        <div className='listSkills'>
+      <div className='contentPerks'>
+        <div className='listPerks'>
           {
             skill_list.map((object) => (
-              <div className='listSkillsButton' key={object.key}>
+              <div className='listPerksButton' key={object.key}>
                 <button name={object.key} onClick={clickHandler}>{object.key}</button>
               </div>
             ))
@@ -51,6 +53,27 @@ const MePart = () => {
             <p>{desc}</p>
             <div className="topRightLine"></div>
           </div>
+        </div>
+      </div>
+    );
+  }
+
+  const AboutMe = () => {
+    return (
+      <div className='contentMe'>
+        <div className='leftAboutMe'>
+          <div className='titleMe'>
+            <p>Jose Miguel Gonzalez</p>
+            <p>Vault 21 - ID143</p>
+          </div>
+          <div className='imageDescMe'>
+            <img src={image}/>
+          </div>
+        </div>
+        <div className='descriptionMe'>
+          <div className="topLeftLine"></div>
+            <p>Esto es un texto de ejemplo</p>
+          <div className="topRightLine"></div>
         </div>
       </div>
     );
@@ -98,53 +121,43 @@ const MePart = () => {
 
   const clickHandlerOption = (e) => {
     if("Perks" == e.target.name){
+      setOp("Perks")
+    } else if("mySelf" == e.target.name){
+      setOp("mySelf")
+    } else if("aboutMe" == e.target.name){
+      setOp("aboutMe")
+    }
+  }
+
+  const Returnable = () => {
+    if("Perks" == op){
       console.log("Perks")
       return( 
         <Perks/>
       )
       
-    } else if("mySelf" == e.target.name){
+    } else if("mySelf" == op){
       console.log("mySelf")
       return(
         <MySelf/>
+      )
+    } else if("aboutMe" == op){
+      console.log("aboutMe")
+      return(
+        <AboutMe/>
       )
     }
   }
 
   return (
-    <div className='pip'>
-      <div className="vaultboy">
-        <div className="bar1"></div>
-        <div className="bar2"></div>
-        <div className="bar3"></div>
-        <div className="bar4"></div>
-        <div className="bar5"></div>
-        <div className="bar6"></div>
+    <div className='MeOptions'>
+      <div className="MeAsk">
+        <button name="mySelf" onClick={clickHandlerOption}>Jose</button>
+        <button name="Perks" onClick={clickHandlerOption}>Perks</button>
+        <button name="aboutMe" onClick={clickHandlerOption}>About me</button>
       </div>
-      <div className='statsMe'>
-        <div className='gun'>
-          <img src={gun}/>
-        </div>
-        <div className='aim'>
-          <img src={aim}/>
-          <p>89</p>
-        </div>
-
-        <div className='helmet'>
-          <img src={helmet}/>
-        </div>
-        <div className='shield'>
-          <img src={shield}/>
-          <p>89</p>
-        </div>
-        <div className='rad'>
-          <img src={rad}/>
-          <p>89</p>
-        </div>
-        <div className='energy'>
-          <img src={energy}/>
-          <p>89</p>
-        </div>
+      <div className='regresador'>
+        <Returnable/>
       </div>
     </div>  
   );
